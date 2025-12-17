44f25538590f812f94e13011b20f4c4cadcca219
chandra prakash <chandraprakashagr@hotmail.com>
2025-12-17 18:56:52 +0530

    Changed the error messages

--- ecommerce.Core/Validators/RegisterRequestValidator.cs
+++ ecommerce.Core/Validators/RegisterRequestValidator.cs
@@ -13,7 +13,7 @@ namespace ecommerce.Core.Validators
         {
 
             RuleFor(temp => temp.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Email is not correct format");
-            RuleFor(temp => temp.PersonName).NotEmpty().WithMessage("Person name is 1,required").Length(1, 50).WithMessage("Length should be 1 to 50");
+            RuleFor(temp => temp.PersonName).NotEmpty().WithMessage("Person name is required").Length(1, 50).WithMessage("Length should be 1 to 50");
             RuleFor(temp => temp.Password).NotEmpty().WithMessage("Password is required");
             RuleFor(temp => temp.Gender).IsInEnum().WithMessage("its outside the range");
         }           
